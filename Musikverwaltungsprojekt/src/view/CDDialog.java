215c795dab2fb67d445459fc14e82ca7247968af
Maukoell <Mauricio.koell@students.htlinn.ac.at>
2017-05-12 10:12:56 +0200

    CDDialog GUI fertig

--- Musikverwaltungsprojekt/src/view/CDDialog.java
+++ Musikverwaltungsprojekt/src/view/CDDialog.java
@@ -12,10 +12,15 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JTextField;
+import javax.swing.JSeparator;
 
 public class CDDialog extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
@@ -34,15 +39,40 @@ public class CDDialog extends JDialog {
 	 * Create the dialog.
 	 */
 	public CDDialog() {
-		setBounds(100, 100, 552, 401);
+		setBounds(100, 100, 476, 233);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		contentPanel.setLayout(new MigLayout("", "[259.00][263.00]", "[99.00][99.00][112.00]"));
+		contentPanel.setLayout(new MigLayout("", "[259.00][263.00,grow]", "[99.00][99.00][99][]"));
 		{
 			JLabel lblTitel = new JLabel("Titel:");
 			lblTitel.setFont(new Font("Tahoma", Font.BOLD, 15));
 			contentPanel.add(lblTitel, "cell 0 0,alignx center,aligny center");
+		}
+		{
+			textField = new JTextField();
+			contentPanel.add(textField, "cell 1 0,alignx center,aligny center");
+			textField.setColumns(20);
+		}
+		{
+			JLabel lblVerlag = new JLabel("Verlag:");
+			lblVerlag.setFont(new Font("Tahoma", Font.BOLD, 15));
+			contentPanel.add(lblVerlag, "cell 0 1,alignx center,aligny center");
+		}
+		{
+			textField_1 = new JTextField();
+			textField_1.setColumns(20);
+			contentPanel.add(textField_1, "cell 1 1,alignx center,aligny center");
+		}
+		{
+			JLabel lblErscheinungsdatum = new JLabel("Erscheinungsdatum:");
+			lblErscheinungsdatum.setFont(new Font("Tahoma", Font.BOLD, 15));
+			contentPanel.add(lblErscheinungsdatum, "cell 0 2,alignx center,aligny center");
+		}
+		{
+			textField_2 = new JTextField();
+			textField_2.setColumns(20);
+			contentPanel.add(textField_2, "cell 1 2,alignx center,aligny center");
 		}
 		{
 			JPanel buttonPane = new JPanel();
