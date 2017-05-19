faaa5c3e9f4808a840493ad95e8a109ec76ab473
jodli <jodli@DESKTOP-I3BBKAN>
2017-05-19 10:33:11 +0200

    SongInformationen

--- Musikverwaltungsprojekt/view/SongInformation.java
+++ Musikverwaltungsprojekt/view/SongInformation.java
@@ -6,6 +6,10 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 public class SongInformation extends JFrame {
 
@@ -37,6 +41,82 @@ public class SongInformation extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.WEST);
+		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1, BorderLayout.NORTH);
+		
+		JPanel panel_2 = new JPanel();
+		contentPane.add(panel_2, BorderLayout.EAST);
+		
+		JPanel panel_3 = new JPanel();
+		contentPane.add(panel_3, BorderLayout.SOUTH);
+		
+		JPanel panel_4 = new JPanel();
+		contentPane.add(panel_4, BorderLayout.CENTER);
+		GridBagLayout gbl_panel_4 = new GridBagLayout();
+		gbl_panel_4.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel_4.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel_4.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_4.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panel_4.setLayout(gbl_panel_4);
+		
+		JLabel lblNewLabel = new JLabel("Name:");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 2;
+		gbc_lblNewLabel.gridy = 0;
+		panel_4.add(lblNewLabel, gbc_lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel_1.gridx = 6;
+		gbc_lblNewLabel_1.gridy = 0;
+		panel_4.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		JLabel lblDauer = new JLabel("Dauer:");
+		GridBagConstraints gbc_lblDauer = new GridBagConstraints();
+		gbc_lblDauer.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDauer.gridx = 2;
+		gbc_lblDauer.gridy = 2;
+		panel_4.add(lblDauer, gbc_lblDauer);
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel_2.gridx = 6;
+		gbc_lblNewLabel_2.gridy = 2;
+		panel_4.add(lblNewLabel_2, gbc_lblNewLabel_2);
+		
+		JLabel lblInterpret = new JLabel("Interpret:");
+		GridBagConstraints gbc_lblInterpret = new GridBagConstraints();
+		gbc_lblInterpret.insets = new Insets(0, 0, 5, 5);
+		gbc_lblInterpret.gridx = 2;
+		gbc_lblInterpret.gridy = 4;
+		panel_4.add(lblInterpret, gbc_lblInterpret);
+		
+		JLabel lblNewLabel_3 = new JLabel("New label");
+		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel_3.gridx = 6;
+		gbc_lblNewLabel_3.gridy = 4;
+		panel_4.add(lblNewLabel_3, gbc_lblNewLabel_3);
+		
+		JLabel lblCd = new JLabel("CD:");
+		GridBagConstraints gbc_lblCd = new GridBagConstraints();
+		gbc_lblCd.insets = new Insets(0, 0, 0, 5);
+		gbc_lblCd.gridx = 2;
+		gbc_lblCd.gridy = 6;
+		panel_4.add(lblCd, gbc_lblCd);
+		
+		JLabel lblNewLabel_4 = new JLabel("New label");
+		GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
+		gbc_lblNewLabel_4.gridx = 6;
+		gbc_lblNewLabel_4.gridy = 6;
+		panel_4.add(lblNewLabel_4, gbc_lblNewLabel_4);
 	}
 
 }
